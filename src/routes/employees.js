c261d657ec76ffc26dedcc2760f291ccb3184559
fjlacruz <idsistemas15@gmail.com>
2020-01-29 11:44:58 -0300

    Subiendo proyecto

---
+++ src/routes/employees.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+
+const mysqlConnection  = require('../database.js');
+
+// GET all Employees
+router.get('/test/:id?', (req, res) => {
+
+ const  id  = req.params;
+_id=null;
+//console.log(id); 
+
+if(id){
+query='SELECT * FROM employee WHERE id = id';
+console.log(query);
+}
+
+if(!id){
+query='SELECT * FROM employee';
+console.log(query);
+}
+
+
+  mysqlConnection.query(query, (err, rows, fields) => {
+    if(!err) {
+      res.json(rows[0]);
+    } else {
+      console.log(err);
+    }
+  });  
+});
+
+// GET An Employee
+router.get('/:id?', (req, res) => {
+
+  const  id  = req.params; 
+query='SELECT * FROM employee WHERE id = ?';
+
+
+  mysqlConnection.query(query, [id], (err, rows, fields) => {
+    if (!err) {
+      res.json(rows[0]);
+console.log(id)
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// DELETE An Employee
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('DELETE FROM employee WHERE id = ?', [id], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Employee Deleted'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+// INSERT An Employee
+router.post('/', (req, res) => {
+  const {id, name, salary} = req.body;
+  console.log(id, name, salary);
+  const query = `
+    SET @id = ?;
+    SET @name = ?;
+    SET @salary = ?;
+    CALL employeeAddOrEdit(@id, @name, @salary);
+  `;
+  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Employeed Saved'});
+    } else {
+      console.log(err);
+    }
+  });
+
+});
+
+router.put('/:id', (req, res) => {
+  const { name, salary } = req.body;
+  const { id } = req.params;
+  const query = `
+    SET @id = ?;
+    SET @name = ?;
+    SET @salary = ?;
+    CALL employeeAddOrEdit(@id, @name, @salary);
+  `;
+  mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+    if(!err) {
+      res.json({status: 'Employee Updated'});
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+module.exports = router;
